031a2a4ebf5e50fc850d3a2d59f7df3ff3dc944e
ADS <50706050+PZD2937@users.noreply.github.com>
2022-10-21 11:21:19 +0800

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import babel from 'rollup-plugin-babel'
+import filesize from 'rollup-plugin-filesize'
+import uglify from 'rollup-plugin-uglify'
+
+export default {
+  entry: 'src/index.js',
+  format: 'cjs',
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    uglify(),
+    filesize()
+  ],
+  dest: 'dist/workerPool.js',
+  sourceMap: true
+}
